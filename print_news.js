a0e724f3cd8eff18c3e26027f86c6c83a209b546
ayush <ayush@email.com>
2022-12-11 17:50:42 +0530

    optimising code and concise

--- print_news.js
+++ print_news.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 import express from 'express';
-import path from 'path'
-import { fileURLToPath } from 'url'
+import path from 'path';
+import { fileURLToPath} from 'url';
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,28 +12,30 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
-// search string in url 
-const url = 'https://newsapi.org/v2/everything?q=delhi&sortBy=popularity&apiKey=091b3cb6469f43caacf630221ca121e6&searchIn=title,description'
+// mention user specified search keyword here
+let search_query = 'world+cup'
+let API_KEY = '091b3cb6469f43caacf630221ca121e6'
 
+// all headlines
+// const url = `https://newsapi.org/v2/everything?sortBy=popularity&apiKey=091b3cb6469f43caacf630221ca121e6&searchIn=title&language=en&q=${search_query}`
+
+// top headlines
+const url = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}&q=${search_query}`
 
 let res = await fetch(url)
 let data = await res.json()
-
 
 app.get('/', (req,res)=>{
     res.send('welcome to newspaper.com')
 })
 
 app.get('/:news_id', (req,res) => {
-
     let k = req.params.news_id
 
     let titles = data['articles'][k]['title']
     let descriptions = data['articles'][k]['description']
     let urls = data['articles'][k]['url']
     let image_url = data['articles'][k]['urlToImage']
-
-    // res.send(`${titles} <hr> ${descriptions} <p> <a href="${urls}" target="_blank"> ${urls} </a>`)
 
     res.render('home', {news:titles, desc:descriptions, link: urls, image_url})
 })

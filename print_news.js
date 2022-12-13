07ce1279663ab5999e1c5945382d12e25ff5e72a
ayush <ayush@email.com>
2022-12-13 19:33:35 +0530

    moved api key to secret environment

--- print_news.js
+++ print_news.js
@@ -2,7 +2,8 @@ import fetch from "node-fetch";
 import express from 'express';
 import path from 'path';
 import { fileURLToPath} from 'url';
-
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,20 +14,21 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
 // mention user specified search keyword here
-let search_query = 'india'
-let API_KEY = '091b3cb6469f43caacf630221ca121e6'
+let search_query = 'tesla'
+const api_key = process.env.API_KEY
 
 // all headlines
-// const url = `https://newsapi.org/v2/everything?sortBy=popularity&apiKey=091b3cb6469f43caacf630221ca121e6&searchIn=title&language=en&q=${search_query}`
+const url = `https://newsapi.org/v2/everything?sortBy=popularity&apiKey=${api_key}&searchIn=title&language=en&q=${search_query}`
 
 // top headlines
-const url = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}&q=${search_query}`
+// const url = `https://newsapi.org/v2/top-headlines?apiKey=${api_key}&q=${search_query}`
 
 let res = await fetch(url)
 let data = await res.json()
 
 app.get('/', (req,res)=>{
-    res.send('welcome to newspaper.com')
+    // res.send('welcome to newspaper.com')
+    res.render('default')
 })
 
 app.get('/:news_id', (req,res) => {
